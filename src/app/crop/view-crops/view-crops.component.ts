c71979102299a54f54f425ce9a0e9219e2c22ff1
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 12:17:42 +0530

    Add files via upload

---
+++ src/app/crop/view-crops/view-crops.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-crops',
+  templateUrl: './view-crops.component.html',
+  styleUrls: ['./view-crops.component.css']
+})
+export class ViewCropsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
