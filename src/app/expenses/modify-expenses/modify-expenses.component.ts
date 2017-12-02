bf733af6a0ad4371fd055a0e018e7959bd07fb25
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 12:28:04 +0530

    Add files via upload

---
+++ src/app/expenses/modify-expenses/modify-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-expenses',
+  templateUrl: './modify-expenses.component.html',
+  styleUrls: ['./modify-expenses.component.css']
+})
+export class ModifyExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
