be4014ad6242572827ca1ffc3752975706c90db8
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 12:37:12 +0530

    Add files via upload

---
+++ src/app/farm/update-farm/update-farm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-farm',
+  templateUrl: './update-farm.component.html',
+  styleUrls: ['./update-farm.component.css']
+})
+export class UpdateFarmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
