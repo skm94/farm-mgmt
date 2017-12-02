81fc169ef774c2c5e45480626d4dd12074c8127f
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 14:03:56 +0530

    Add files via upload

---
+++ src/app/input/add-input/add-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-input',
+  templateUrl: './add-input.component.html',
+  styleUrls: ['./add-input.component.css']
+})
+export class AddInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
