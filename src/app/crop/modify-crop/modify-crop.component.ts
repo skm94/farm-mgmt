bc2d6ae241ea483cf350fd1ab3b7d32862e48f5e
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 12:14:33 +0530

    Add files via upload

---
+++ src/app/crop/modify-crop/modify-crop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-crop',
+  templateUrl: './modify-crop.component.html',
+  styleUrls: ['./modify-crop.component.css']
+})
+export class ModifyCropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
