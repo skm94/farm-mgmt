73acbb2d4d5aca603362e4982cc2045512c1d012
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 14:14:00 +0530

    Add files via upload

---
+++ src/app/plantation/add-plantation/add-plantation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-plantation',
+  templateUrl: './add-plantation.component.html',
+  styleUrls: ['./add-plantation.component.css']
+})
+export class AddPlantationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
