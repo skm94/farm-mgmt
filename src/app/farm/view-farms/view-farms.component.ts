99d13cb5d1e62a846d0de0bd791a3e479c9d37ca
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 12:39:09 +0530

    Add files via upload

---
+++ src/app/farm/view-farms/view-farms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-farms',
+  templateUrl: './view-farms.component.html',
+  styleUrls: ['./view-farms.component.css']
+})
+export class ViewFarmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
