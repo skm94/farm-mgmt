2ac557162a97c54c98e8b86f871bacc8594d833f
Salkhan Majhi <skm94@users.noreply.github.com>
2017-12-02 14:07:28 +0530

    Add files via upload

---
+++ src/app/input/view-inputs/view-inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-inputs',
+  templateUrl: './view-inputs.component.html',
+  styleUrls: ['./view-inputs.component.css']
+})
+export class ViewInputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
